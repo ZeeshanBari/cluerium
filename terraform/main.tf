fc87c72dee0f0e0b5f86506a0e784ee3f2d362db
ZeeshanBari <zeeshan.bari13@gmail.com>
2024-03-17 00:57:26 -0400

    update terraform - include docker container ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,15 @@
 ## Create ECR repository 
-resource "aws_ecr_repository" "ecr_repo" {
-  name = "ecr_repo"
+resource "aws_ecr_repository" "cluerium_ecr_repo" {
+  name = "cluerium_ecr_repo"
 }
 
 ## Build frontend docker image and push to ECR
-resource "docker_registry_image" "frontend" {
-  name = "${aws_ecr_repository.ecr_repo.repository_url}:latest"
+resource "docker_registry_image" "cluerium_frontend" {
+  name = "${aws_ecr_repository.cluerium_ecr_repo.repository_url}-frontend:latest"
 
   build {
-    context    = "../frontend"
-    dockerfile = "Dockerfile"
+    context    =  "${path.cwd}/../frontend"
+    dockerfile = "Dockerfile.multistage"
   }
 }
 
